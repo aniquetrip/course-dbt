051dcb9b5065d150416924c95ffcbae585b894bb
Anique Trip <anique.trip@mollie.com>
2023-04-22 10:38:20 +0000

    Update files

--- greenery/models/marts/product/fact_products.sql
+++ greenery/models/marts/product/fact_products.sql
@@ -1,4 +1,4 @@
--- mart model to identify daily statistics per product
+-- mart model to determine and join daily statistics per product
 
 {{ 
     config(
@@ -6,37 +6,28 @@
 ) 
 }}
 
-WITH events_session_orders AS (
-    SELECT * FROM {{ ref('int_events_sessions_orders') }}
+WITH orders_products AS (
+    SELECT * FROM {{ ref('int_orders_products') }}
 )
 
-, events_sessions_products AS (
-   SELECT * FROM {{ ref('int_events_sessions_products') }}
+, page_views_products AS (
+   SELECT * FROM {{ ref('int_page_views_products') }}
 )
 
--- joining these together so we can determine which page views resulted in an order
-, combined AS (
-SELECT 
-    events_sessions_products.*
-    , CASE WHEN total_add_to_carts = 0 THEN 0 ELSE total_orders END AS total_orders
-FROM events_sessions_products
-LEFT JOIN events_session_orders
-ON events_sessions_products.session_id = events_session_orders.session_id AND events_sessions_products.created_day = events_session_orders.created_day 
-WHERE events_sessions_products.product_id is not null
+, products AS (
+   SELECT * FROM {{ ref('stg_products') }}
 )
-
--- aggregate and define metrics per day per product 
 
 SELECT 
-combined.product_id
+page_views_products.product_id
 , products.name
-, created_day
-, SUM(total_page_views) AS total_page_views
-, SUM(total_add_to_carts) AS total_add_to_carts
-, SUM(total_orders) AS total_orders
-, ROUND(SUM(total_orders) / SUM(total_page_views),2) AS conversion_rate --this is the conversion rate of page views into orders
-FROM combined
-LEFT JOIN DEV_DB.DBT_ANIQUETRIPMOLLIECOM.STG_PRODUCTS AS products
-ON combined.product_id = products.product_id
-GROUP BY 1,2, 3
-order by product_id, created_day
+, page_views_products.created_day
+, page_views_products.total_page_views
+, page_views_products.total_add_to_carts
+, orders_products.total_orders
+, ROUND((total_orders) / (total_page_views),2) AS conversion_rate -- conversion rate of page views into orders
+FROM page_views_products
+LEFT JOIN orders_products 
+    ON page_views_products.product_id = orders_products.product_id AND page_views_products.created_day = orders_products.created_day
+LEFT JOIN products
+    ON orders_products.product_id = products.product_id
